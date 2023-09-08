d5bcb238d8284ec4981d338fbeba5cb9f21325aa
= <steven.serruya@gmail.com>
2023-09-07 21:22:56 -0500

    functioning version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoFavButton from 'components/PhotoFavButton';
 import PhotoList from 'components/PhotoList';
+import TopicList from 'components/TopicList';
 
 // Note: Rendering a single component to build components in isolation
 
@@ -44,7 +45,7 @@ const App = () => {
   return (
     <div className="App">
       <PhotoList />
-
+      <TopicList />
 
 
 
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
   {
@@ -23,7 +24,11 @@ const sampleDataForTopicList = [
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {sampleDataForTopicList.map((topic) => {
+        return (
+          <TopicListItem key={topic.id} label={topic.title} />
+        );
+      })}
     </div>
   );
 };
