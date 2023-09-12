9ce5ff0d7544d1d522e5628b7f7f0790c6afdeaa
= <steven.serruya@gmail.com>
2023-09-11 20:32:01 -0500

    no rerendering during handleclick

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -5,6 +5,11 @@ import "../styles/TopicListItem.scss";
 
 
 const TopicListItem = ({ label, handleTopicClick, id }) => {
+  const handleClick = (e, id) => {
+    e.preventDefault();  // Prevents the default action of the <a> tag
+    handleTopicClick(id);
+  };
+
   return (
     <div className="topic-list__item">
       <a href="#" onClick={(e) => {
