14f6612e5e8f8752b41cbed1e8371391012ad4e8
= <steven.serruya@gmail.com>
2023-09-12 21:04:42 -0500

    added fav to modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,27 +4,57 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 import PhotoList from 'components/PhotoList';
+import PhotoFavButton from 'components/PhotoFavButton';
 
 // PhotoDetailsModal component to display details of a selected photo
 
-const PhotoDetailsModal = ({ onClose, photo, favourites, toggleFavourite }) => {
+const PhotoDetailsModal = ({ onClose, photo, favourites, toggleFavourite, id, isFav, toggleModal }) => {
+  const handleToggleFavourite = () => {
+    toggleFavourite(id);
 
+  };
 
   return (
 
     <div className="photo-details-modal">
 
       {/* Button to close the modal */}
+      <div className="photo-details-modal__top-bar">
 
-      <button className="photo-details-modal__close-button" onClick={() => onClose(null)}>
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
-      <br></br>
+
+        <button className="photo-details-modal__close-button" onClick={() => onClose(null)}>
+          <img src={closeSymbol} alt="close symbol" />
+        </button>
+
+
+      </div>
 
       {/* Display the selected photo */}
+      <div className="photo-details-modal__images">
+        <PhotoFavButton
+          photoId={id}
+          isFav={isFav}
+          toggleFavourite={handleToggleFavourite}
+          onClick={() => toggleFavourite(photo.id)}
+          toggleModal={toggleModal}
+        />
+        <img src={photo.urls.regular} alt={`Photo by ${photo.user.username}`} className="photo-details-modal__image" />
+      </div>
 
-      <img src={photo.urls.regular} alt={`Photo by ${photo.user.username}`} className="photo-details-modal__image" />
 
+
+      <div className="photo-details-modal__photographer-details">
+        <div className="profile-picture">
+          <img src={photo.user.profile} alt={`Profile ${id}`} className="photo-list__user-profile" />
+        </div>
+        <div className="photo-list__user-info">{photo.user.username}
+          <div className="photo-list__user-location">
+            {photo.location.city}, {photo.location.country}
+          </div>
+        </div>
+
+
+      </div>
       {/* Display similar photos in a list */}
 
       <div className="photo-details-modal__images">
